eaec748a3cf5c48eb77e0ef241c0ab3376e5c0a1
pboyd <pboyd@localhost>
2011-10-04 17:43:16 -0400

    added main program genstruct.py

---
+++ genstruct.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+"""
+GenStruct -- Generation of Structures for fapping.
+"""
+
+__version__ = "$Revision: 1 $"
+
+import subprocess
+import sys
+import numpy
+
+class Structure(object):
+    """
+    Structure manipulations and calculations
+    """
+
+
+def main():
+    """Default if run as an executable"""
+
+
+if __name__ == '__main__':
+    main()
